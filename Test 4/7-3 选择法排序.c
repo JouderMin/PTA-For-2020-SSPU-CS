3baab07c62ecff1ff691ce38402e31485619ae88
JouderMin <joudermin@foxmail.com>
2020-12-14 19:31:59 +0800

    7-3

---
+++ Test 4/7-3 选择法排序.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void) {
+    int n, temp, index;
+    scanf("%d", &n);
+    int num[n];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &num[i]);
+    }
+
+    for (int i = 0; i < n - 1; i++) {
+        index = i;
+        for (int j = i + 1; j < n; j++) {
+            if (num[j] > num[index])
+                index = j;
+        }
+        temp       = num[index];
+        num[index] = num[i];
+        num[i]     = temp;
+    }
+    for (int k = 0; k < n; k++) {
+        printf("%d", num[k]);
+        if (k < n - 1) {
+            printf(" ");
+        }
+    }
+
+    return 0;
+}
