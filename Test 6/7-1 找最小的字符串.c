cd056f36589476cfa3a238d463f8c4dcc1cc3430
JouderMin <joudermin@foxmail.com>
2020-12-21 13:59:58 +0800

    add answer

---
+++ Test 6/7-1 找最小的字符串.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    int num;
+    char min[81], temp[81] = {0};
+
+    scanf("%d\n", &num);
+    gets(min);
+    for (int i = 0; i < num - 1; i++) {
+        gets(temp);
+        if (strcmp(min, temp) >= 0) {
+            strcpy(min, temp);
+        }
+        strcpy(temp,"\0");
+    }
+    printf("Min is: %s", min);
+
+    return 0;
+}
