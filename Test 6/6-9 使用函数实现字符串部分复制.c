017b441425d1d35973ccc4f7a15b2e1f029f3e9b
JouderMin <joudermin@foxmail.com>
2020-12-21 12:57:55 +0800

    add answer

---
+++ Test 6/6-9 使用函数实现字符串部分复制.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#define MAXN 20
+
+void strmcpy(char *t, int m, char *s);
+void ReadString(char s[]); /* 由裁判实现，略去不表 */
+
+int main(void) {
+    char t[MAXN], s[MAXN];
+    int m;
+
+    scanf("%d\n", &m);
+    ReadString(t);
+    strmcpy(t, m, s);
+    printf("%s\n", s);
+
+    return 0;
+}
+
+#include <string.h>
+
+void strmcpy(char *t, int m, char *s) {
+    int tLength = strlen(t), i;
+
+    if (m <= tLength) {
+        for (i = 0; t[i + m - 1]; i++) {
+            s[i] = t[i + m - 1];
+        }
+        s[i] = '\0';
+    } else {
+        strcpy(s, "\0");
+    }
+}
